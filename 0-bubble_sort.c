69f35b849d81d37ee19c7808e9346d96ee9d59e2
Naftali Holland <naftaliholland01@gmail.com>
2023-10-23 19:23:22 +0300

    Added a flag to check for already sorted array

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,21 +12,26 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
+	char swapped;
 
 	if (array == NULL || size < 1)
 		return;
 
+	swapped = 0;
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
+				swapped = 1;
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
+		if (swapped == 0)
+			break;
 	}
 }
